03f9547e7d2d0397ee99d219c41a10d4dd96e78f
DARSHIT DESAI <desai0681@gmail.com>
2023-04-08 01:12:20 -0400

    Update a_star_DarshitMiteshkumar_Shivam.py
    ros changes

--- a_star_DarshitMiteshkumar_Shivam.py
+++ a_star_DarshitMiteshkumar_Shivam.py
@@ -5,7 +5,7 @@ import numpy as np
 from queue import PriorityQueue
 import math
 import time
-
+from geometry_msgs.msg import Twist
 
 
 
@@ -129,7 +129,7 @@ def move_robot(robot,curr_theta, costtocome):
         c2g=math.dist((x_t,y_t),(goal_x,goal_y))
         robot_position=(round(x_t),round(y_t))
         total_cost_come=c2c+costtocome
-        new_nodes.append([c2g+total_cost_come,total_cost_come,c2g,robot_position,t_t,points])
+        new_nodes.append([c2g+total_cost_come,total_cost_come,c2g,robot_position,t_t,points,action])
     return new_nodes
 
 def new_node(new_node_list):
@@ -140,12 +140,14 @@ def new_node(new_node_list):
     t=new_node_list[4]
     x,y=new_pos 
     points=new_node_list[5]
+    r_p_m = new_node_list[6]
+    print("current rpm",r_p_m)
     if (((x>0 and x<600) and (y>0 and y<200))==True):
         if ( not(any(screen.get_at((a,b))!=white for a,b in points)) and screen.get_at((new_pos)) == white and not (new_pos in check_closed_list)):
             if not (new_pos in global_dict):
                 global node_index
                 node_index += 1
-                global_dict[new_pos]=[total_cost,cost_to_come,cost_to_goal,node_index,info[3],new_pos,t,points]
+                global_dict[new_pos]=[total_cost,cost_to_come,cost_to_goal,node_index,info[3],new_pos,t,points,r_p_m]
                 open_list.put(global_dict[new_pos])
                 dict_vector[info[5]]=points
             else:
@@ -155,6 +157,7 @@ def new_node(new_node_list):
                     global_dict[new_pos][0]=total_cost
                     global_dict[new_pos][6]=t
                     global_dict[new_pos][7]=points
+                    global_dict[new_pos][8]=r_p_m
 
 ctc_node=0  # cost to come for start node
 ctc_goal=math.dist((start_x,start_y),(goal_x,goal_y)) # cost to goal for the start node
@@ -163,11 +166,12 @@ node_index=0 # Index of the current node
 closed_list={} # dictionary to store information about the current node
 check_closed_list={} # dictionary to store the nodes to check if nodes present in closed list
 open_list=PriorityQueue() # list the store nodes and pop them according to priority
-info=[ctc_goal+ctc_node,ctc_node,ctc_goal,node_index,parent_node_index,robot_start_position,start_theta] # list to save all info of a node
+rp_m = [0,0]
+pts = []
+info=[ctc_goal+ctc_node,ctc_node,ctc_goal,node_index,parent_node_index,robot_start_position,start_theta,pts,rp_m] # list to save all info of a node
 open_list.put(info)
-
 global_dict={} # global dictionary to reference all the information for nodes in the open list and to update the information
-global_dict[robot_start_position]=[ctc_goal+ctc_node,ctc_node,ctc_goal,node_index,parent_node_index,robot_start_position,start_theta]
+global_dict[robot_start_position]=[ctc_goal+ctc_node,ctc_node,ctc_goal,node_index,parent_node_index,robot_start_position,start_theta,pts,rp_m]
 
 
 start_time=time.time()# to store start time of the algorithm
@@ -183,20 +187,21 @@ while True and end_loop!=1:
         break
     
     info=open_list.get()
+    
     new_nodes=move_robot(info[5],info[6],info[1])
     # append the node to node list                                               
     for i in range(0,8):
         if(new_nodes[i][2]<=0.5):
             print("goal reached")
             print("I breaked at: ",i)
-            closed_list[node_index+i+1]=[new_nodes[i][0]+info[1],new_nodes[i][1]+info[1],new_nodes[i][2],info[3],new_nodes[i][3],new_nodes[i][4]]
-            global_dict[new_nodes[i][3]]=[new_nodes[i][0],new_nodes[i][1],new_nodes[i][2],node_index+i+1,info[3],new_nodes[i][3],new_nodes[i][4],new_nodes[i][-1]]
+            closed_list[node_index+i+1]=[new_nodes[i][0]+info[1],new_nodes[i][1]+info[1],new_nodes[i][2],info[3],new_nodes[i][3],new_nodes[i][4],new_nodes[i][-1]]
+            global_dict[new_nodes[i][3]]=[new_nodes[i][0],new_nodes[i][1],new_nodes[i][2],node_index+i+1,info[3],new_nodes[i][3],new_nodes[i][4],new_nodes[i][5],new_nodes[i][-1]]
             goal_node=node_index+i+1
             end_loop=1
             break 
         new_node(new_nodes[i])
                                     
-    closed_list[info[3]]=[info[0],info[1],info[2],info[4],info[5],info[6]]
+    closed_list[info[3]]=[info[0],info[1],info[2],info[4],info[5],info[6],info[7]]
     print(closed_list[info[3]])
     check_closed_list[info[5]]=None
     
@@ -212,15 +217,21 @@ pyg.display.update()
 
 # Find the path form start to goal
 path = []
+path_theta_list = []
+RPM_list = []
 if goal_node!=None:
     st_time = time.time()
     print("The final goal node is given by: ",goal_node)
     while goal_node is not None:
         goal_node_parent = closed_list[goal_node][3]
         path.append(closed_list[goal_node][4])
+        path_theta_list.append(closed_list[goal_node][5])
+        RPM_list.append(closed_list[goal_node][6])
         goal_node=goal_node_parent        
     # reverse the path list to get the correct order of nodes
     path.reverse()
+    path_theta_list.reverse()
+    RPM_list.reverse()
     et_time = time.time()
     print("Total time taken for backtracking:",et_time-st_time)
     print("****** The optimum path is ****",path)
@@ -252,6 +263,8 @@ for i in range(len(path)):
 # for key, value in closed_list.items():
 #     print(f"{key}: {value}")
 # Set the caption of the screen
+print("RPM list length",len(RPM_list))
+print(RPM_list)
 pyg.display.set_caption('A* Visualization Map')
 pyg.display.update()
 pyg.time.wait(1)
@@ -262,4 +275,52 @@ while running:
 		# Check for QUIT event	
 		if event.type == pyg.QUIT:
 			running = False
-                       
+
+for i in range(0,len(path)):
+    xt,yt = path[i]
+    yf = 200-yt
+    yf1 = yf -100
+    xf = xt-50
+    xf1 = xf/100
+    yf2 = yf1/100
+    path[i]=(xf1,yf2)
+print("I'm done till here")
+
+
+import rospy
+
+rospy.init_node('ROS_AStar', anonymous=True)
+pub_vel = rospy.Publisher('cmd_vel',Twist,queue_size=100)
+msg = Twist()
+turtle_model = rospy.get_param("model","burger")
+msg.linear.x = 0.0
+msg.linear.y = 0.0
+msg.linear.z = 0.0
+msg.angular.x = 0.0
+msg.angular.y = 0.0
+msg.angular.z = 0.0
+
+pub_vel.publish(msg)
+r = rospy.Rate(10)
+t =1 #Time step
+for i in range(len(path)):
+    x1,y1 = path[i] 
+    x2,y2 = path[i+1]
+    t1 = path_theta_list[i]
+    t2 = path_theta_list[i+1]
+    dx = x2-x1
+    dy = y2-y1
+    vx = dx/t
+    vy = dy/t
+    dw = t2-t1
+    wt = dw/t
+    vel_inp = math.sqrt(vx**2+vy**2)
+    msg.linear.x = vel_inp
+    msg.angular.z = wt
+    pub_vel.publish(msg)
+    r.sleep()
+
+
+
+
+
